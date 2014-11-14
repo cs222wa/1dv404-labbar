b73775827181fc7fe1da52bf09b8b52ba1d60567
cs222wa <cs222wa@studen.lnu.se>
2014-11-14 15:47:50 +0100

    Made inital planning for my labs. Solved lab 1.a and added references in planning document.

---
+++ 1/1a/RaknaA/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaknaA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Var god ange valfri mening: ");
+            string sentence = Console.ReadLine();
+            if (sentence == null)
+            {
+                Console.WriteLine("Du måste skriva något i textfältet.");
+            }
+            int smallA = TextTool.CharInString(sentence, "A");
+            int bigA = TextTool.CharInString(sentence, "a");
+            Console.WriteLine("Antal a: {0} \nAntal A: {1}", smallA, bigA);
+        }
+    }
+}
