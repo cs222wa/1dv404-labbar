f71ced238d681a868e275104c38d3dea820db933
cs222wa <cs222wa@studen.lnu.se>
2014-11-17 09:00:38 +0100

    Began lab 1b.

---
+++ 1/1b/RaknaSiffror/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaknaSiffror
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            int odd;
+            int even;
+
+           // kriv ett program RaknaSiffror som för ett godtyckligt positivt heltal N (läses in från 
+           // tangentbordet) skriver ut antalet nollor, antalet udda siffror och antalet jämna siffror. En 
+           // körning kan se ut enligt följande: 
+           // Mata in ett heltal: 6789500 Nollor: 2 Udda: 3 Jämna: 2
+        }
+    }
+}
