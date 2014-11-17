c413d386650e305dabf9b6c65a2d6c51d9ffaede
cs222wa <cs222wa@studen.lnu.se>
2014-11-17 09:36:05 +0100

    Got program to write out if the number is odd/even when entered one at a time. Input format is faulty.

--- 1/1b/RaknaSiffror/Program.cs
+++ 1/1b/RaknaSiffror/Program.cs
@@ -10,15 +10,45 @@ namespace RaknaSiffror
     {
         static void Main(string[] args)
         {
-            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine("Var god ange 10 siffror.");
 
-            int odd;
-            int even;
+            int[] input = new int[10]; // Tar emot siffror frn användare
+            for (int i = 0; i < 10; i++)
+            {
+                input[i] = int.Parse(Console.ReadLine());
+                if (IsOdd(input[i]))
+                {
+                    Console.WriteLine("Siffran {0} är ojämn", input[i]);
+                }
+                if (!IsOdd(input[i]))
+                {
+                    Console.WriteLine("Siffran {0} är jämn", input[i]);
+                }
+            }
+        }
+            public static bool IsOdd(int value)
+            {
+	        return value % 2 != 0;
+            }
+            
 
-           // kriv ett program RaknaSiffror som för ett godtyckligt positivt heltal N (läses in från 
-           // tangentbordet) skriver ut antalet nollor, antalet udda siffror och antalet jämna siffror. En 
-           // körning kan se ut enligt följande: 
-           // Mata in ett heltal: 6789500 Nollor: 2 Udda: 3 Jämna: 2
+            //int odd;
+            //int even;
+
+            //if (isOdd(input)) ;      //Om siffran är ojämn skriv ut antalet
+            //{
+            //}
+            //if (!isOdd(input)) ;    //Om siffran är jämn skriv ut antalet
+            //{
+            //}
+
+
+
+
+            // kriv ett program RaknaSiffror som för ett godtyckligt positivt heltal N (läses in från 
+            // tangentbordet) skriver ut antalet nollor, antalet udda siffror och antalet jämna siffror. En 
+            // körning kan se ut enligt följande: 
+            // Mata in ett heltal: 6789500 Nollor: 2 Udda: 3 Jämna: 2
         }
     }
-}
+
