029a9f76f8467af8d29ad077a095bd0a240b9340
cs222wa <cs222wa@studen.lnu.se>
2014-11-21 15:17:51 +0100

    Finished assignment 3b.

--- 3/3B/FractionalMath/Fraction.cs
+++ 3/3B/FractionalMath/Fraction.cs
@@ -25,26 +25,105 @@ namespace FractionalMath
     /// </summary>
     class Fraction
     {
-        //Konstruktor som skapar och initialiserar ett nytt bråktal.
-        //Om nämnaren är noll ska ett felmeddelande lämnas.
+        private int _numerator;
+        private int _denominator;
 
-        //Metod getNumerator returnerar täljare
+        /// <summary>
+        ///Metod getNumerator returnerar täljare som ska  vara ett heltal
+        /// </summary>
+        public int getNumerator
+        {
+            get { return _numerator; }
+            set { _numerator = value; }
+        }
+        /// <summary>
+        ///Metod getDenominator returnerar nämnare som ska vara ett heltal
+        /// </summary>
+        public int getDenominator
+        {
+            get { return _denominator; }
+            set { _denominator = value; } 
+        }
+        /// <summary>
+        ///Konstruktor som skapar och initialiserar ett nytt bråktal.
+        /// </summary>
+        /// <param name="numerator"></param>
+        /// <param name="denominator"></param>
+        public Fraction(int numerator, int denominator) 
+        {
+            getNumerator = numerator;
+            getDenominator = denominator;
 
-        //Metod getDenominator returnerar nämnare
-
-        //metoden isNegative ger true om det är ett negativt bråktal
-
-        //Metoden add  utför bråktalsoperation på två bråk och returnerar ett nytt bråktal.
-        //Bestäm själv ett lämpligt sätt att hantera de 
-        //fall där någon av de inblandade bråktalen har noll i nämnaren. 
-
-        //metoden multiply utför bråktalsoperation på två bråk och returnerar ett nytt bråktal.
-        //Bestäm själv ett lämpligt sätt att hantera de 
-        //fall där någon av de inblandade bråktalen har noll i nämnaren. 
-
-       // • isEqualTo som jämför två Fraction-instanser och ser om de representerar samma 
-       //bråktal. 
-
-       // • toString som returnerar en strängrepresentation av bråktalet på form Täljare/Nämnare. 
+            if (getDenominator == 0) //Om nämnaren är noll ska ett felmeddelande lämnas.
+            {
+                throw new FormatException("Din nämnare får inte ha värdet 0. Försök igen.");
+            }
+        }
+        /// <summary>
+        ///Metoden add  utför bråktalsoperation på två bråk och returnerar ett nytt bråktal.
+        ///Bestäm själv ett lämpligt sätt att hantera de 
+        ///fall där någon av de inblandade bråktalen har noll i nämnaren. 
+        /// </summary>
+        /// <param name="one"></param>
+        /// <param name="two"></param>
+        /// <returns></returns>
+        public static Fraction operator +(Fraction one, Fraction two)
+        {
+            return new Fraction(one.getNumerator * two.getDenominator + two.getNumerator * one.getDenominator,
+               one.getDenominator * two.getDenominator);
+        }
+        /// <summary>
+        ///Metoden multiply utför bråktalsoperation på två bråk och returnerar ett nytt bråktal.
+        ///Bestäm själv ett lämpligt sätt att hantera de 
+        ///fall där någon av de inblandade bråktalen har noll i nämnaren. 
+        /// </summary>
+        /// <param name="one"></param>
+        /// <param name="two"></param>
+        /// <returns></returns>
+        public static Fraction operator *(Fraction one, Fraction two)
+        {
+            return new Fraction(one.getNumerator * two.getNumerator, one.getDenominator * two.getDenominator);
+        }
+        /// <summary>
+        ///Metoden isNegative returnerar true om det är ett negativt bråktal
+        /// </summary>
+        /// <returns></returns>
+        public bool isNegative()
+        {
+            if (0 > getDenominator || getNumerator < 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        ///isEqualTo som jämför två Fraction-instanser och ser om de representerar samma 
+        ///bråktal. 
+        /// </summary>
+        /// <param name="one"></param>
+        /// <param name="two"></param>
+        /// <returns></returns>
+        public bool isEqual(Fraction a, Fraction b)
+        {
+            if ((decimal)a.getNumerator/a.getDenominator == (decimal)b.getNumerator/b.getDenominator)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// toString som returnerar en strängrepresentation av bråktalet på form Täljare/Nämnare. 
+        /// </summary>
+        /// <returns></returns>
+        public string toString()
+        {
+            return string.Format("{0}/{1}", getNumerator, getDenominator);
+        }
     }
 }
--- 3/3B/FractionalMath/Program.cs
+++ 3/3B/FractionalMath/Program.cs
@@ -10,9 +10,91 @@ namespace FractionalMath
     {
         static void Main(string[] args)
         {
-            Fraction math = new Fraction();
+            try
+            {
+                Fraction one = new Fraction(3, 4);
+                Fraction two = new Fraction(1, 2);
+                Fraction three = new Fraction(5, 5);
+                Fraction four = new Fraction(2, 2);
+                Fraction threeFour = new Fraction(2, 2);
+                Fraction five = new Fraction(-2, 6);
 
-            Console.WriteLine(math);
+                //Fraction six = new Fraction(5, 0);  // <--- uncomment to display error message.
+
+                Console.WriteLine("-------------------------");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Add");
+                Console.ResetColor();
+                Console.WriteLine("{0} + {1} = {2}", one.toString(), two.toString(), (one + two).toString());
+                Console.WriteLine();
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Multiply");
+                Console.ResetColor();
+                Console.WriteLine("{0} * {1} = {2}", one.toString(), two.toString(), (one * two).toString());
+                Console.WriteLine();
+
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Is Equal");
+                Console.ResetColor();
+                Console.WriteLine("Representerar bråktalet {0} samma decimaltal som {1}?", three.toString(), four.toString());
+                if (threeFour.isEqual(three, four) == true)
+                {
+                    Console.WriteLine("Ja, det båda talen representerar samma decimaltal.");
+                }
+                else
+                {
+                    Console.WriteLine("Nej, det båda talen representerar inte samma decimaltal.");
+                }
+                Console.WriteLine();
+                Console.WriteLine("Representerar bråktalet {0} samma decimaltal som {1}?", one.toString(), two.toString());
+                if (three.isEqual(one, two) == true)
+                {
+                    Console.WriteLine("Ja, det båda talen representerar samma decimaltal.");
+                }
+                else
+                {
+                    Console.WriteLine("Nej, det båda talen representerar inte samma decimaltal.");
+                }
+                Console.WriteLine();
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Is Negative");
+                Console.ResetColor();
+                Console.WriteLine("Betraktas bråktalet {0} som negativt?", five.toString());
+                if (five.isNegative() == true)
+                {
+                    Console.WriteLine("Ja, det aktuella talet betraktas som negativt.");
+                }
+                else
+                {
+                    Console.WriteLine("Nej, det aktuella talet betraktas inte som negativt.");
+                }
+                Console.WriteLine();
+                Console.WriteLine("Betraktas bråktalet {0} som negativt?", two.toString());
+                if (two.isNegative() == true)
+                {
+                    Console.WriteLine("Ja, det aktuella talet betraktas som negativt.");
+                }
+                else
+                {
+                    Console.WriteLine("Nej, det aktuella talet betraktas inte som negativt.");
+                }
+                Console.WriteLine();
+                Console.WriteLine("-------------------------");
+            }
+            catch (Exception e)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(e.Message);
+                Console.ResetColor();
+            }
+        }
+
+        private static void negativeMessage()
+        {
+            throw new NotImplementedException();
         }
     }
 }
